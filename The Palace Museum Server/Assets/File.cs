c309e9955fe395dc7d1430adf66ad320d7bf65df
Huahaha233 <2539791707@qq.com>
2019-02-22 17:31:03 +0800

    基于Unity3D的古村落数字档案展示平台服务端

---
+++ The Palace Museum Server/Assets/File.cs
@@ -0,0 +1,137 @@
+﻿using UnityEngine;
+using MySql.Data.MySqlClient;
+using System;
+using System.IO;
+using UnityEditor;
+using Newtonsoft.Json;
+
+public class File : MonoBehaviour {
+
+    private string obj_json;
+    //private byte[] imagedata = new byte[8000000];
+	// Use this for initialization
+	void Start () {
+       
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    
+
+    public void AddInfoToMysql(byte[] imagedata)//向数据库中添加数据
+    {
+        /*当存储的文件过大时，一定要在MySQL的my.ini文件中修改max_allowed_packet的值，并重新启动MySQL*/
+        string sql = "insert into model(oldvillage_ID,oldvillage_name,oldvillage_data,imagedata,wechat,notes) values('1','test','" + obj_json + "',@imagedata,'是','测试')";
+        //ChangeMysql(sql);
+
+        string Mysql_str = "server=localhost;user id=root;password=123456;database=oldvillage;Sslmode=none";
+
+        MySqlConnection myconn = new MySqlConnection(Mysql_str);
+        try
+        {
+            myconn.Open();
+            MySqlCommand mycomm = new MySqlCommand(sql, myconn);
+            mycomm.Parameters.AddWithValue("@imagedata", imagedata);
+            Debug.Log("赋值完成");
+            mycomm.ExecuteNonQuery();
+        }
+        catch (MySqlException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        finally
+        {
+            myconn.Close();
+        }
+
+    }
+
+
+
+    public static void ChangeMysql(string Mysql_change)//对数据库进行操作，通过其他函数传入的参数进行操作
+    {
+        string Mysql_str = "server=localhost;user id=root;password=123456;database=oldvillage";
+        MySqlConnection myconn = new MySqlConnection(Mysql_str);
+        try
+        {
+            myconn.Open();
+            MySqlCommand mycomm = new MySqlCommand(Mysql_change, myconn);
+            mycomm.ExecuteNonQuery();
+        }
+        catch (MySqlException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        finally
+        {
+            myconn.Close();
+        }
+    }
+    /*打开文件夹，选择obj文件，将obj文件转化为byte[]格式，保存至数据库*/
+    public void OpenFile()//打开文件夹
+    {
+        //string objPath = "";
+        string extion = "obj";
+        string path = "";
+        path = EditorUtility.OpenFilePanel("Load obj of Directory", Application.dataPath, extion);
+        if (path != null)
+        {
+            Debug.Log("获取文件路径成功：" + path);
+            //objPath = path;
+        }
+        string obj = System.IO.File.ReadAllText(path);//obj文件可以以文本的形式打开，此方法可以获取obj文本形式的值
+
+        /*将字符串形式的obj文件转化为json格式*/
+        obj_json = JsonConvert.SerializeObject(obj);
+        
+        ImageToByte();
+        
+    }
+
+    public void ImageToByte()//将图转化为byte[]格式
+    {
+        
+        string extion = "png,jpg";
+        string path = "";
+        path = EditorUtility.OpenFilePanel("Load Images of Directory", Application.dataPath, extion);
+        if (path != null)
+        {
+            Debug.Log("获取文件路径成功：" + path);
+        }
+        
+        FileStream fs = new FileStream(path, FileMode.Open);
+        //BinaryReader br = new BinaryReader(fs);
+        //byte[] imagedata = br.ReadBytes((int)fs.Length); //将流读入到字节数组中
+        byte[] imagedata = new byte[fs.Length];
+        fs.Read(imagedata, 0, imagedata.Length);
+        fs.Close();
+        Debug.Log(imagedata.Length);
+        AddInfoToMysql(imagedata);
+
+        
+    }
+    //public static byte[] ObjectToBytes(object obj)//将obj文件转化为byte[]格式
+    //{
+
+    //    using (MemoryStream ms = new MemoryStream())
+    //    {
+    //        IFormatter formatter = new BinaryFormatter();
+    //        formatter.Serialize(ms, obj);
+    //        return ms.GetBuffer();
+    //    }
+    //}
+    //public static object ByteToObject(byte[] Bytes)//将byte[]转化为obj格式
+    //{
+    //    using (MemoryStream ms = new MemoryStream(Bytes))
+    //    {
+    //        IFormatter formatter = new BinaryFormatter();
+    //        return formatter.Deserialize(ms);
+    //    }
+    //}
+}
+
+
+
+
---
+++ The Palace Museum Server/Assets/Login.cs
@@ -0,0 +1,132 @@
+﻿using MySql.Data.MySqlClient;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class Login : MonoBehaviour {
+    public InputField user;
+    public InputField psw;
+	// Use this for initialization
+	void Start () {
+        CreatDatabase();
+        CreatTable();
+        AddInfoToMysql();/*添加的管理员账户与密码只是为了测试*/
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    
+    public static void CreatDatabase()//创建数据库
+    {
+        string creatdatabase = "Data Source=localhost;Persist Security Info=yes;UserId=root;PWD=123456";
+        string MysqlStatment = "CREATE DATABASE IF NOT EXISTS oldvillage DEFAULT CHARSET utf8 COLLATE utf8_general_ci;";
+        MySqlConnection conn = new MySqlConnection(creatdatabase);
+        try
+        {
+            conn.Open();
+            MySqlCommand comm = new MySqlCommand(MysqlStatment, conn);
+            comm.ExecuteNonQuery();
+
+        }
+        catch
+        {
+
+        }
+        finally
+        {
+            conn.Close();
+        }
+
+    }
+    public static void CreatTable()//创建数据库表
+    {
+        string sql = "CREATE TABLE IF NOT EXISTS `model` (`oldvillage_ID` INT UNSIGNED AUTO_INCREMENT,  `oldvillage_name` CHAR(10) NOT NULL, `oldvillage_data` MEDIUMBLOB NOT NULL, `imagedata` MEDIUMBLOB NOT NULL, `wechat` CHAR(1) NOT NULL,`notes` CHAR(50),PRIMARY KEY(`oldvillage_ID`)) DEFAULT CHARSET = utf8";
+        ChangeMysql(sql);
+        string sql1 = "CREATE TABLE IF NOT EXISTS `admin` (`Admin_id` VARCHAR(2) NOT NULL,  `Admin_name` CHAR(10) NOT NULL, `Admin_sex` CHAR(1) NOT NULL, `Admin_psw` CHAR(10) NOT NULL, `Admin_conn` CHAR(11) NOT NULL,`notes` CHAR(50),PRIMARY KEY(`Admin_ID`)) DEFAULT CHARSET = utf8";
+        ChangeMysql(sql1);
+
+    }
+
+    public static void ChangeMysql(string Mysql_change)//对数据库进行操作，通过其他函数传入的参数进行操作
+    {
+        string Mysql_str = "server=localhost;user id=root;password=123456;database=oldvillage";
+        MySqlConnection myconn = new MySqlConnection(Mysql_str);
+        try
+        {
+            myconn.Open();
+            MySqlCommand mycomm = new MySqlCommand(Mysql_change, myconn);
+            mycomm.ExecuteNonQuery();
+        }
+        catch (MySqlException ex)
+        {
+           
+        }
+        finally
+        {
+            myconn.Close();
+        }
+    }
+
+    public void AddInfoToMysql()//向数据库中添加数据
+    {
+        /*添加的管理员账户与密码只是为了测试*/
+        string sql = "insert into admin values('1','text','男','1','1','测试')";
+        ChangeMysql(sql);
+    }
+
+    public int ReadMysql(string name, string psw)
+    {
+        int find = 0;
+        string connetStr = "server=localhost;user id=root;password=123456;database=oldvillage";//注意Sslmode要赋值为none，否则无法连接到数据库   
+        MySqlConnection conn = new MySqlConnection(connetStr);
+        MySqlCommand command = null;
+        MySqlDataReader dataReader = null;
+        try
+        {
+            conn.Open();
+            command = conn.CreateCommand();
+            command.CommandText = "SELECT * FROM admin";
+            dataReader = command.ExecuteReader();
+            while (dataReader.Read())
+            {
+                if (name == dataReader.GetString(0) && psw == dataReader.GetString(3))
+                {
+                    find = 1;//find为1时，证明数据库中用户名已存在
+                    break;
+                }
+
+            }
+        }
+        catch
+        {
+            Debug.Log("未能连接到数据库！");
+        }
+        finally { conn.Close(); }
+        return find;
+
+    }
+
+    public void Judge()
+    {
+        
+        if (ReadMysql(user.text, psw.text) == 1)
+        {
+            transform.Find("Tips").GetComponent<Text>().text = "登录成功！";
+            Invoke("ToScene", 1f);//延迟1秒后跳转页面
+        }
+        else
+        {
+            transform.Find("Tips").GetComponent<Text>().text = "信息错误！";
+        }
+    }
+
+    public void ToScene()
+    {
+        SceneManager.LoadScene("OpenFile");
+    }
+}
